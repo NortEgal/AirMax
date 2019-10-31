4f326bde6efdb6021dc2ecf6f5dca8cdd700c520
Norrrbert <dimon007.zamorin@gmail.com>
2019-10-31 17:30:39 +0500

    fix theme?/

--- src/js/theme.js
+++ src/js/theme.js
@@ -5,15 +5,19 @@ if (theme == 'light') {
 if (theme == 'dark') {
 	document.documentElement.setAttribute('theme','dark');
 };
+if (document.documentElement.getAttribute('theme') == '')
+{
+	document.documentElement.setAttribute('theme', 'light');
+}
 $(document).on('click','#switch-theme', function() {
 	if (document.documentElement.hasAttribute('theme')) {
 		if (document.documentElement.getAttribute('theme') == 'dark') {
 			document.documentElement.setAttribute('theme', 'light');
-			document.cookie = "theme=light; domain=localhost; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT;";
+			document.cookie = "theme=light; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT;";
 		}
 		else if (document.documentElement.getAttribute('theme') == 'light') {
 			document.documentElement.setAttribute('theme', 'dark');
-			document.cookie = "theme=dark; domain=localhost; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT;";
+			document.cookie = "theme=dark; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT;";
 		}
 	}
 })
