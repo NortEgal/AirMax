46c371791474ea3e971a18f6fc6e0305d69d740e
aerok925 <sokolov.maksim.1976@mail.ru>
2019-11-06 03:22:18 +0500

    добавление рейсов

---
+++ src/php/add.php
@@ -0,0 +1,17 @@
+<?php
+    require('bitconnect.php');
+    
+    
+    if ($_GET(['t']) == "add_account")
+    {
+        $formplane = $_POST["form-plane"]
+        $formwhere_from = $_POST["formwhere_from"]
+        $formwhere_to = $_POST["formwhere_to"]
+        $formtime_departure = $_POST["formtime_departure"]
+        $formtime_arrival = $_POST["formtime_arrival"]
+        $formprice = $_POST["formprice"]
+
+        $query = "INSERT INTO flight VALUES (NULL, '$formplane', '$formwhere_from', '', '$formwhere_to', '', '$formtime_departure', '$formtime_arrival', NULL, '0', '$formprice');";
+		$result = mysqli_query($connection, $query);
+    }
+?>
