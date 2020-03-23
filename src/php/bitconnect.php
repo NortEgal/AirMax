1b19c430243510dea002fc92889c5b80fb5b7661
Norrrbert <40669749+Norrrbert@users.noreply.github.com>
2020-03-23 14:41:13 +0500

    reconnect

--- src/php/bitconnect.php
+++ src/php/bitconnect.php
@@ -1,13 +1,14 @@
 <?php
-	define("DB_SERVER", "VH274.spaceweb.ru");
-	define("DB_USER", "sokolovma2");
-	define("DB_PASS", "2140174Qq");
-	define("DB_NAME", "sokolovma2");
+	// define("DB_SERVER", "VH274.spaceweb.ru");
+	// define("DB_USER", "sokolovma2");
+	// define("DB_PASS", "2140174Qq");
+	// define("DB_NAME", "sokolovma2");
 
 	// define("DB_SERVER", "localhost");
-	// define("DB_USER", "allah");
-	// define("DB_PASS", "YV0QXuCo8pGRFtbCGrcX");
-	// define("DB_NAME", "Allah");
+	 define("DB_SERVER", "allah.xyecoc.club");
+	 define("DB_USER", "allah");
+	 define("DB_PASS", "YV0QXuCo8pGRFtbCGrcX");
+	 define("DB_NAME", "Allah");
 
 	$connection = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 
